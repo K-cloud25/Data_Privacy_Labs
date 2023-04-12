6a8d1c367cb405283b49ba29c33a9667a627f3f2
K-Cloud25 <keyuroak@gmail.com>
2023-04-12 12:18:16 +0530

    code done

--- Lab-AES/KeyGeneration.py
+++ Lab-AES/KeyGeneration.py
@@ -8,10 +8,13 @@ class KeyGeneration:
         file = open(r"C:\\Users\\keyur\\Lecs\\Lab-AES\\key.txt","r")
         string = bytes(file.readline(),"utf-8")        
         key = []
-
+        print(len(string))
         for i in range(0,4):
-            temp_key = [string[4*(i)],string[4*(i+1)],string[4*(i+2)],string[4*(i+3)]]
+            temp_key = [string[4*i],string[4*i+1],string[4*i+2],string[4*i+3]]
             self.key.append(temp_key)
+
+        for i in range(4,44):
+            temp = key[i -1]
 
     def retKeys(self):
         return self.key
